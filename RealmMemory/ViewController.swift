b728f4ce0735a9eb9cbcbc20bb8140f5b8a84526
Dave Morgereth <dmorgereth@tandemdiabetes.com>
2022-09-30 14:33:53 -0700

    Eliminated embedded object - not needed to reproduce the issue

--- RealmMemory/ViewController.swift
+++ RealmMemory/ViewController.swift
@@ -109,8 +109,7 @@ class ViewController: UIViewController {
         let realm = try! Realm()
 
         for i in (0...2000) {
-            let embedded = Embedded()
-            let obj = RealmObj(started_at: Date().addingTimeInterval(Double(i * 2000)), ended_at: Date().addingTimeInterval(Double(i * 2000)), embedded: embedded)
+            let obj = RealmObj(started_at: Date().addingTimeInterval(Double(i * 2000)), ended_at: Date().addingTimeInterval(Double(i * 2000)))
             try! realm.write {
                 realm.add(obj)
             }
@@ -155,7 +154,6 @@ class RealmObj: Object {
     @Persisted(indexed: true) var started_at: Date = Date()
     @Persisted(indexed: true) var ended_at: Date = Date()
     @Persisted var title: String = ""
-    @Persisted var embedded: Embedded?
 
     override static func primaryKey() -> String? {
        return "id"
@@ -169,102 +167,11 @@ class RealmObj: Object {
         title = ""
     }
 
-    init(started_at: Date, ended_at: Date, title: String = "", embedded: Embedded?) {
+    init(started_at: Date, ended_at: Date, title: String = "") {
         super.init()
         id = UUID().uuidString
         self.started_at = started_at
         self.ended_at = ended_at
         self.title = title
-        self.embedded = embedded
     }
 }
-
-class Embedded: EmbeddedObject {
-    @Persisted var notes: String
-
-    override init() {
-        super.init()
-        notes = ""
-    }
-}
-
-/*
-class RealmObj1: Object {
-    @Persisted(indexed: true) var id: String = ""
-    @Persisted var clientStgId: String = ""
-    @Persisted(indexed: true) var event_type: String = ""
-    @Persisted var account_id: Int = 0
-    @Persisted var source: String?
-    @Persisted var source_id: String?
-    @Persisted var db_source_child_ids: String = ""      // Don't mess with this directly
-    @Persisted var source_subtype: String?
-    @Persisted(indexed: true) var started_at: Date = Date()
-    @Persisted(indexed: true) var ended_at: Date = Date()
-    @Persisted var duration_in_seconds: Int = 0
-    @Persisted var title: String?
-    @Persisted var notes: String?
-    @Persisted var pg_id: String = ""
-    @Persisted var deleted: Bool = false
-    @Persisted var created_at: Date = Date()
-    @Persisted var updated_at: Date = Date()
-    @Persisted var needsSaveToServer: Bool = false
-    @Persisted var needsSaveToHealthKit: Bool = false
-    @Persisted var embedded: Embedded?
-//    @Persisted var basal_rate: SMSubEventEntryBasalRate?
-//    @Persisted var device_change: SMSubEventEntryDeviceChange?
-//    @Persisted var exercise: SMSubEventEntryExercise?
-//    @Persisted var food: SMSubEventEntryFood?
-//    @Persisted var insulin: SMSubEventEntryInsulin?
-//    @Persisted var liquid: SMSubEventEntryLiquid?
-//    @Persisted var med: SMSubEventEntryMed?
-//    @Persisted var glucose: SMSubEventEntryGlucose?
-//    @Persisted var schedule_change: SMSubEventEntryScheduleChange?
-
-    var source_child_ids: [String] {
-        get {
-            return db_source_child_ids.components(separatedBy: ",")
-        }
-        set {
-            db_source_child_ids = newValue.joined(separator: ",")
-        }
-    }
-
-    required init(id: String = "", event_type: String, account_id: Int = 0, source: String? = nil, source_id: String? = nil, source_child_ids: [String] = [], source_subtype: String? = nil, started_at: Date = Date(), ended_at: Date = Date(), duration_in_seconds: Int = 0, title: String? = nil, notes: String? = nil, pg_id: String = "", deleted: Bool = false, created_at: Date = Date(), updated_at: Date? = nil, embedded: Embedded? = nil) {
-
-        super.init()
-        self.id = id
-        self.event_type = event_type
-        self.account_id = account_id
-        self.source = source
-        self.source_id = source_id
-        self.source_child_ids = source_child_ids
-        self.source_subtype = source_subtype
-        self.started_at = started_at
-        self.ended_at = ended_at
-        self.duration_in_seconds = duration_in_seconds
-        self.title = title
-        self.notes = notes
-        self.pg_id = pg_id
-        self.deleted = deleted
-        self.created_at = created_at
-        self.updated_at = updated_at ?? created_at
-//        self.basal_rate = basal_rate
-//        self.device_change = device_change
-//        self.exercise = exercise
-//        self.food = food
-//        self.insulin = insulin
-//        self.liquid = liquid
-//        self.med = med
-//        self.glucose = glucose
-//        self.schedule_change = schedule_change
-        self.embedded = embedded
-        self.needsSaveToServer = false
-        self.needsSaveToHealthKit = false
-    }
-
-    override init() {
-        super.init()
-    }
-
-}
-*/
